47cb5ab16fc5e585c6a3b5427f7d0c36d5d42a98
WJJ-Company <1780409106@qq.com>
2023-12-25 16:25:50 +0800

    Update favicon and add new keyboard icon Remove unused components and update App.vue Remove HelloWorld and update WelcomeItem.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,11 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script lang="ts">
+import Keys from "./components/KeyboardKey.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
 
   <main>
-    <TheWelcome />
+    <Keys/>
   </main>
 </template>
 
---
+++ src/components/KeyboardKey.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="Key">
+        A
+    </div>
+</template>
+<script setup lang="ts">
+
+</script>
+<style>
+#key {
+    width: 50px;
+    height: 50px;
+    background-color: #000;
+}
+</style>
