a73b70bfc73219390b11f40446db0d4e13b6b598
WJJ-Company <1780409106@qq.com>
2023-12-29 16:40:35 +0800

    modified:   src/App.vue 	modified:   src/components/KeyboardKey.vue 	new file:   src/mocks/KeyboardData.json 	new file:   src/mocks/KeyboardData.ts

--- src/components/KeyboardKey.vue
+++ src/components/KeyboardKey.vue
@@ -1,15 +1,28 @@
 <template>
-    <div id="Key">
-        A
+    <div class="key">
+        {{keyName}}
     </div>
 </template>
-<script setup lang="ts">
-
+<script lang="ts">
+export default {
+    name: "KeyWorld",
+}
 </script>
-<style>
-#key {
+<script setup lang="ts">
+defineProps({
+    keyName: String,
+    keyOrigin: String,
+})
+</script>
+
+<style scoped>
+.key {
     width: 50px;
     height: 50px;
-    background-color: #000;
+    border: #000 1px solid;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: 700;
 }
 </style>
---
+++ src/mocks/KeyboardData.ts
@@ -0,0 +1,8 @@
+export interface KeyboardData {
+    
+}
+
+export class KeyboardData{
+ 
+    keyboardName!: string;
+}
